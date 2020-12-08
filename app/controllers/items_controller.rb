0e6d3cd877492c5d202881cfe00218a5f002ea95
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-08 20:11:05 +0900

    出品画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end
+  private
+  def item_params
+    params.require(:item).permit(:name, :content, :price, :joint_buying, :category_id, :status_id, :bear_price_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
   end
 end
