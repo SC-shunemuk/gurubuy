0f515fde06b5a5ad53e559a55e75b0b4ced35835
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-09 12:38:53 +0900

    選択項目の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :status
+  belongs_to :category
+  belongs_to :shipping_method
+  belongs_to :prefecture
 end
