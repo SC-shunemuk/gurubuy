cc10406ae50852c781bd3b88e8e462772b0dae9e
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-10 19:24:57 +0900

    モデルのテストコードの記入

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image               { Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images/anomaly-602401.png')) }
+    name                { Faker::Name.initials(number: 2) }
+    content             { Faker::Lorem.sentence }
+    price               { 450 }
+    category_id         { 2 }
+    status_id           { 2 }
+    joint_buying_id     { 2 }
+    shipping_method_id  { 2 }
+    prefecture_id       { 2 }
+    association :user
   end
 end
