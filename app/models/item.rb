ca5d92a28829b7faa21121ad5e2b5044a9c9e1c1
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-10 18:04:19 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,23 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :joint_buying
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :content, length: { maximum: 1000 }
+    validates :category_id
+    validates :status_id
+    validates :joint_buying
+    validates :prefecture_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      format: { with: /\A[0-9]+\z/ }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :joint_buying
+    validates :prefecture_id
+  end
 end
