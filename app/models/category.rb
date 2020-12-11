7d22db4bc48305690ca961fc416d2e285b7261a6
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-11 09:47:23 +0900

    カテゴリーの微調整

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,13 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: 'ハンドメイド' },
-    { id: 3, name: 'レディース' },
-    { id: 4, name: 'メンズ' },
+    { id: 2, name: 'アクセサリー' },
+    { id: 3, name: 'インテリア' },
+    { id: 4, name: 'ファッション' },
     { id: 5, name: 'ベビー・キッズ' },
-    { id: 6, name: '本・音楽・ゲーム' },
-    { id: 7, name: 'おもちゃ・ホビー・グッズ' },
-    { id: 8, name: '家電・スマホ・カメラ' },
-    { id: 9, name: 'スポーツ・レジャー' },
-    { id: 10, name: 'インテリア・住まい・小物' },
-    { id: 11, name: 'その他' }
+    { id: 6, name: 'ファッション小物' },
+    { id: 7, name: '雑貨' },
+    { id: 8, name: 'その他' }
 
   ]
   include ActiveHash::Associations
