b1d9c242ba525a1913a7740fc79c3ea77d69f5fc
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-14 23:10:20 +0900

    viewの処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,18 @@
 class OrdersController < ApplicationController
-  
+  before_action :set_item, only: :index
+
+  def index
+    price = @item.price
+    @count = @item.joint_buying.id
+    @count.times do |n|
+      @price = price * 0.95
+      price = @price
+    end
+  end
+
+
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
