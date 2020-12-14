aeafbc0fad7de9761c672429756dbbc47d1fa4c3
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-14 23:47:26 +0900

    orderのバリデーション

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,15 @@ class Order < ApplicationRecord
   belongs_to :item
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+
+  with_options presence: true do
+    validates :address_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :address_number
+    validates :tel, format: { with: /\A\d{10,11}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :user_id
+    validates :item_id
+  end
+
 end
