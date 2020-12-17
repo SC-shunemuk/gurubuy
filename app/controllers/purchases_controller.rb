04ff9a8a7038c7c7156ce2e69d9a9688619824a2
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-17 23:58:49 +0900

    payipの準備

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,24 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
   def index
+    @purchase = Purchase.new
+  end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:item_id])
     @price = @item.price
     @count = @item.exhibit_number
@@ -8,4 +26,17 @@ class PurchasesController < ApplicationController
       @price = @price*= 0.95
     end
   end
+  def purchase_params
+    params.require(:purchase).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
