e3f3f7abd62d33a63c3548b6108f539819ff0d31
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-17 23:08:14 +0900

    購入金額の割引の表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,5 +2,10 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
+    @price = @item.price
+    @count = @item.exhibit_number
+    @count.times do
+      @price = @price*= 0.95
+    end
   end
 end
--- db/migrate/20201208102738_create_items.rb
+++ db/migrate/20201208102738_create_items.rb
@@ -8,6 +8,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :category_id,        null: false
       t.integer :shipping_method_id, null: false
       t.integer :prefecture_id,      null: false
+      t.integer :exhibit_number
       t.references :user,            foreign_key: true
       t.timestamps
     end
