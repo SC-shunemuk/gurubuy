11121cccc4d359d31f2328647f1750d0d4fca8fd
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-17 11:47:02 +0900

    マイページのviewの整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,8 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @items = Item.where(user_id: current_user.id)
+    order = Order.where(user_id: user)
+    @buying_items = Item.joins(:orders).where(joint_buying_id: 0)
+
   end
 end
