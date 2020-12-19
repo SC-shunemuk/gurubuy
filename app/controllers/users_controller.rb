5479c5e60d5b4f7611a572dcb5163634d6708cbd
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-19 23:26:58 +0900

    マイページの決済の非表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @items = Item.where(user_id: current_user.id)
-    @buying_items = Item.joins(:orders).where(joint_buying_id: 0)
+    @ordered_items = Item.joins(:orders).where(joint_buying_id: 0)
+    @purchased_items = Item.joins(:purchases)
   end
 end
